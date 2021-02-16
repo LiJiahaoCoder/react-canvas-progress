760c67dd0bddc830e7536a05e58a3f0a044549c7
Jiahao Li <lijiahao_shandong@163.com>
2021-02-16 17:14:59 +0800

    fix: rename epcilon to epsilon

--- src/utils/math.ts
+++ src/utils/math.ts
@@ -1,8 +1,8 @@
 export const { PI, round, min, max, abs } = Math;
 
-export const EPCILON = 0.00001;
+export const EPSILON = 0.00001;
 
-export const equal = (a: number, b: number) => abs(a - b) <= EPCILON;
+export const equal = (a: number, b: number) => abs(a - b) <= EPSILON;
 
 export const computeCos = (
   startAngle: number,
